8e1619661e0d8c57658e478fd144e20a749e3e2f
toseth97 <oluwasheun9721@gmail.com>
2023-10-30 15:43:43 +0100

    image upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 
+import "./static/css/index.css";
+import "./static/css/styles.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { useState } from "react";
+import Cookies from "universal-cookie";
+
+
+
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      
+      
+      </BrowserRouter>
       
     </div>
   );
